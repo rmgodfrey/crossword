ec09f4b089693dd491a24ce72d7a8e1f340df230
Ross Godfrey <ross.m.godfrey@gmail.com>
2024-01-25 17:44:50 -0500

    Focus grid input on control click

--- src/components/Crossword.js
+++ src/components/Crossword.js
@@ -82,6 +82,7 @@ export default function Crossword({
       { check, reveal, clear }[instruction],
     );
     setCellText(newCellText);
+    focusGridInput();
   }
 
   function handleTabKey(direction) {
